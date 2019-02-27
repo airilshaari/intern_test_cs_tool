f4e08016915404ce52d63d3be6490b4316d3f12a
Mohamad Shokal <mhdgsho@gmail.com>
2019-02-27 15:39:31 +0800

    update AppServiceProvider to force https on production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if(config('app.env') === 'prod') {
+            \URL::forceScheme('https');
+        }
     }
 
     /**
