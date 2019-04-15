44999ba731b019c8a05e1e53a0113373dc7f29f3
airil-avana <airil@avana.asia>
2019-04-15 17:19:14 +0800

    date purchased

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -33,7 +33,7 @@ class OrderController extends Controller
         $data = array();
 
         $data['order'] = (new CustomerOrder())->findOrder($order_no, $shop_id, $order_email);
-        $data['order']->date_purchased = Carbon::createFromFormat('Y-m-d H:i:s', $data['order']->date_purchased, 'UTC')->setTimezone(config('app.timezone'))->format('d-m-Y g:iA');
+        //$data['order']->date_purchased = Carbon::createFromFormat('Y-m-d H:i:s', $data['order']->date_purchased, 'UTC')->setTimezone(config('app.timezone'))->format('d-m-Y g:iA');
 
         if ($data['order']) {
             $data['shop_info'] = $data['order']->shopInfo()->first();
